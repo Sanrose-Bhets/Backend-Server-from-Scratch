b04a75c8b5d9cb5bdcc23964d355a2a1d463b12e
Sanrose-Bhets <sanrosebhetuwal@gmail.com>
2026-02-17 20:12:36 +0545

    Configurations of the server and enabling Access through browsers using CORS

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Sanrose-Bhets/Backend-Server-from-Scratch
+
+go 1.25.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+	github.com/go-chi/cors v1.2.2
+)
+
+
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-main (){
-	fmt.println("Hello world")
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT was not found in the environment")
+	}
+
+	fmt.Println("Port: ", portString)
+
+	//creating a new router object with chi router
+	router := chi.NewRouter()
+
+	//enables users to send requests from any browsers
+	//passing cors configurations
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server is Starting on Port: %v", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
